338639a9c5c0dfb98b439c33789f9530c556dcae
Andrew Hodel <andrewhodel@gmail.com>
2021-09-07 22:48:02 +0100

    move data collection to while loop

--- bme280_sensor.py
+++ bme280_sensor.py
@@ -12,11 +12,11 @@ bus = smbus2.SMBus(port)
 
 calibration_params = bme280.load_calibration_params(bus, address)
 
-# the sample method will take a single reading and return a
-# compensated_reading object
-data = bme280.sample(bus, address, calibration_params)
-
 while True:
+
+    # the sample method will take a single reading and return a
+    # compensated_reading object
+    data = bme280.sample(bus, address, calibration_params)
 
     # the compensated_reading class has the following attributes
     print(data.id)
@@ -28,7 +28,7 @@ while True:
     # there is a handy string representation too
     print(data)
 
-    sjson = {"login": "1210_plenum", "key": "asdklhdfga", "collectors": {"ping": [{"host": "temp", "avgRtt": data.temperature, "loss": 0}, {"host": "hum", "avgRtt": data.humidity, "loss": 0}, {"host": "pressure", "avgRtt": data.pressure, "loss": 0}]}}
+    sjson = {"login": "1210_plenum", "key": "LSDKJFGNBjfdaabuebnfbqo4385hg44bg9gsjdefslkihjdlskFHKJSDHGIUFDDbndusfosghiofewhfvoubdsfsbhsofufhndsuodfagasgfgsdwasalreadyviewableincollectthesameasgithub", "collectors": {"ping": [{"host": "temp", "avgRtt": data.temperature, "loss": 0}, {"host": "hum", "avgRtt": data.humidity, "loss": 0}, {"host": "pressure", "avgRtt": data.pressure, "loss": 0}]}}
     json_d = json.dumps(sjson)
 
     resp = urllib2.urlopen(req, json_d, cafile="/etc/__ispapp_co.ca-bundle").read()
