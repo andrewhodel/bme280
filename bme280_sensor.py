d26dd0eef6c62ad7db06605f66c05f7fb65feaa7
Andrew Hodel <andrewhodel@gmail.com>
2021-10-22 17:42:05 +0100

    urllib2 seems to be broken, rewrote with raw tls sockets

--- bme280_sensor.py
+++ bme280_sensor.py
@@ -1,11 +1,12 @@
 import smbus2
 import bme280
 import time
-import urllib2
 import json
 import threading
 import platform
 import os
+import socket
+import ssl
 
 # setup configuration
 intervalS = 20;
@@ -52,8 +53,11 @@ th.daemon = True
 th.start()
 
 # setup url endpoints
-ureq = urllib2.Request("https://" + ispapp_domain + ":" + str(ispapp_port) + "/update", headers={"content-type": "application/json"})
-creq = urllib2.Request("https://" + ispapp_domain + ":" + str(ispapp_port) + "/config", headers={"content-type": "application/json"})
+context = ssl.SSLContext(ssl.PROTOCOL_TLS)
+context.verify_mode = ssl.CERT_REQUIRED
+context.check_hostname = True
+context.load_default_certs()
+context.load_verify_locations(cafile="/etc/__ispapp_co.ca-bundle")
 
 while True:
 
@@ -62,28 +66,40 @@ while True:
     sjson = {"login": "1210_plenum", "key": ispapp_key, "clientInfo": "python2.7 bme280.py", "os": platform.system(), "osVersion": platform.release(), "hardwareMake": "raspberry pi", "hardwareModel": "zero w", "hardwareCpuInfo": platform.machine()}
     json_d = json.dumps(sjson)
 
-    # urllib2.urlopen sends raw text and does not add a trailing newline character
-    json_d += "\r\n"
-
     try:
-        resp = urllib2.urlopen(creq, json_d, cafile="/etc/__ispapp_co.ca-bundle").read()
-    except:
-        print("urllib2.urlopen() reported an error")
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        ssl_sock = context.wrap_socket(s, server_hostname=ispapp_domain)
+        ssl_sock.connect((ispapp_domain, ispapp_port))
+
+        print(json_d)
+
+        ssl_sock.write("POST /config HTTP/1.1\r\nHost: " + ispapp_domain + ":" + str(ispapp_domain) + "\r\nConnection: keep-alive\r\nContent-Type: application/json\r\nContent-Length: " + str(len(json_d)) + "\r\n\r\n" + json_d + "\r\n\r\n")
+
+        resp = ssl_sock.read()
+        ssl_sock.close()
+
+        head = resp.split("\r\n\r\n", 1)
+        resp = head[1]
+        head = head[0]
+
+        r = json.loads(resp)
+
+        if (r.has_key("error")):
+            # if there was an error, try again
+            time.sleep(2)
+            continue
+
+        # valid config request
+        #print(r)
+
+        # set intervalS based on server configuration
+        outageIntervalSeconds = r["host"]["outageIntervalSeconds"]
+
+    except Exception as e:
+        print("error", e)
         time.sleep(2)
         continue
-
-    r = json.loads(resp)
-
-    if (r.has_key("error")):
-        # if there was an error, try again
-        time.sleep(2)
-        continue
-
-    # valid config request
-    #print(r)
-
-    # set intervalS based on server configuration
-    outageIntervalSeconds = r["host"]["outageIntervalSeconds"]
 
     break
 
@@ -95,33 +111,45 @@ while True:
     sjson = {"login": "1210_plenum", "key": ispapp_key, "uptime": int(os.times()[4]), "collectors": {"gauge": [{"name": "temperature", "point": data.temperature}, {"name": "relative humidity", "point": data.humidity}, {"name": "pressure", "point": data.pressure}]}}
     json_d = json.dumps(sjson)
 
-    # urllib2.urlopen sends raw text and does not add a trailing newline character
-    json_d += "\r\n"
-    #print(json_d)
-
     try:
-        resp = urllib2.urlopen(ureq, json_d, cafile="/etc/__ispapp_co.ca-bundle").read()
-        #print(resp)
-    except:
-        print("urllib2.urlopen() reported an error")
-        time.sleep(2)
-        continue
 
-    r = json.loads(resp)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        ssl_sock = context.wrap_socket(s, server_hostname=ispapp_domain)
+        ssl_sock.connect((ispapp_domain, ispapp_port))
 
-    if (r.has_key("error")):
-        # if there was an error, try again
-        time.sleep(2)
-        continue
+        #print(json_d)
 
-    if (r["updateFast"]):
-        print("updateFast is True, setting update interval to 2 seconds")
-        intervalS = 2
-    else:
-        # set update wait based on offset, expect 2 seconds for data collection
-        intervalS = outageIntervalSeconds - r["lastUpdateOffsetSec"] - 2
-        if (intervalS < 2):
+        ssl_sock.write("POST /update HTTP/1.1\r\nHost: " + ispapp_domain + ":" + str(ispapp_domain) + "\r\nConnection: keep-alive\r\nContent-Type: application/json\r\nContent-Length: " + str(len(json_d)) + "\r\n\r\n" + json_d + "\r\n\r\n")
+
+        resp = ssl_sock.read()
+        ssl_sock.close()
+
+        head = resp.split("\r\n\r\n", 1)
+        resp = head[1]
+        head = head[0]
+
+        r = json.loads(resp)
+
+        #print(r)
+
+        if (r.has_key("error")):
+            # if there was an error, try again
+            time.sleep(2)
+            continue
+
+        if (r["updateFast"]):
+            print("updateFast is True, setting update interval to 2 seconds")
             intervalS = 2
-        print("updating in " + str(intervalS) + " seconds")
+        else:
+            # set update wait based on offset, expect 2 seconds for data collection
+            intervalS = outageIntervalSeconds - r["lastUpdateOffsetSec"] - 2
+            if (intervalS < 2):
+                intervalS = 2
+            print("updating in " + str(intervalS) + " seconds")
+
+    except Exception as e:
+        print("/update error", e)
+        time.sleep(2)
+        continue
 
     time.sleep(intervalS)
